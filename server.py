e7d24aac12f979c20a5d517fff208da68bd6e4a1
Edric Chai <53452387+edricjchai@users.noreply.github.com>
2022-03-10 13:53:11 -0500

    Add server file

---
+++ server.py
@@ -0,0 +1,38 @@
+import socket
+#import a library called socket
+
+#address of server we would like to listen
+serverPort = 1234
+serverName = 'localhost'
+#reserved address for local in python
+serverAddress = (serverName, serverPort)
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+#there are different types of sockets we can request
+#tcp vs. udp
+serverSocket.bind(serverAddress)
+#associate server addres with this fresh socket
+serverSocket.listen(1)
+
+#some message for possible debugging:
+print("server is listening")
+client_socket, addr =serverSocket.accept();
+#beyond this point, connection is established,
+#and return value client_socket is the server end's handle
+#of this socket for communicating payload
+while True:
+  #receive a message from client
+  message=client_socket.recv(2048);
+  #process the message
+#checking if message is a legit format
+  #parse the http request header
+  
+  modified_message = message.upper()
+  #send the modified message
+  client_socket.send(modified_message)
+  print("reply sent")
+    #set up a sentinel value, EOF, or -1
+
+client_socket.close()
+  
+  
